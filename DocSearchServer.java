f2f8ac23100c7d3089141973686679118f233613
nak003 <nak003@ucsd.edu>
2022-10-30 23:58:49 -0700

    .

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -13,17 +13,17 @@ class FileHelpers {
     static List<File> getFiles(Path start) throws IOException {
         File f = start.toFile();
         List<File> result = new ArrayList<>();
-        if(f.isDirectory()) {
+        if (f.isDirectory()) {
             File[] paths = f.listFiles();
-            for(File subFile: paths) {
+            for (File subFile : paths) {
                 result.addAll(getFiles(subFile.toPath()));
             }
-        }
-        else {
+        } else {
             result.add(start.toFile());
         }
         return result;
     }
+
     static String readFile(File f) throws IOException {
         return new String(Files.readAllBytes(f.toPath()));
     }
@@ -31,40 +31,40 @@ class FileHelpers {
 
 class Handler implements URLHandler {
     List<File> files;
+
     Handler(String directory) throws IOException {
-      this.files = FileHelpers.getFiles(Paths.get(directory));
+        this.files = FileHelpers.getFiles(Paths.get(directory));
     }
+
     public String handleRequest(URI url) throws IOException {
-       List<File> paths = FileHelpers.getFiles(Paths.get("./technical"));
-       if (url.getPath().equals("/")) {
-           return String.format("There are %d total files to search.", paths.size());
-       } else if (url.getPath().equals("/search")) {
-           String[] parameters = url.getQuery().split("=");
-           if (parameters[0].equals("q")) {
-               String result = "";
-               List<String> foundPaths = new ArrayList<>();
-               for(File f: paths) {
-                   if(FileHelpers.readFile(f).contains(parameters[1])) {
-                       foundPaths.add(f.toString());
-                   }
-               }
-               Collections.sort(foundPaths);
-               result = String.join("\n", foundPaths);
-               return String.format("Found %d paths:\n%s", foundPaths.size(), result);
-           }
-           else {
-               return "Couldn't find query parameter q";
-           }
-       }
-       else {
-           return "Don't know how to handle that path!";
-       }
+        List<File> paths = FileHelpers.getFiles(Paths.get("./technical"));
+        if (url.getPath().equals("/")) {
+            return String.format("There are %d total files to search.", paths.size());
+        } else if (url.getPath().equals("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("q")) {
+                String result = "";
+                List<String> foundPaths = new ArrayList<>();
+                for (File f : paths) {
+                    if (FileHelpers.readFile(f).contains(parameters[1])) {
+                        foundPaths.add(f.toString());
+                    }
+                }
+                Collections.sort(foundPaths);
+                result = String.join("\n", foundPaths);
+                return String.format("Found %d paths:\n%s", foundPaths.size(), result);
+            } else {
+                return "Couldn't find query parameter q";
+            }
+        } else {
+            return "Don't know how to handle that path!";
+        }
     }
 }
 
 class DocSearchServer {
     public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
@@ -74,4 +74,3 @@ class DocSearchServer {
         Server.start(port, new Handler("./technical/"));
     }
 }
-
--- TestDocSearch.java
+++ TestDocSearch.java
@@ -7,21 +7,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TestDocSearch {
-	@Test 
-	public void testIndex() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/");
-    assertEquals("There are 10 total files to search.", h.handleRequest(rootPath));
-	}
-	@Test 
-	public void testSearch() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/search?q=taxation");
-    Path path1 = Paths.get("./technical/biomed/1471-2296-3-3.txt");
-    Path path2 = Paths.get("./technical/government/Gen_Account_Office/d01591sp.txt");
-    Path path3 = Paths.get("./technical/plos/journal.pbio.0020052.txt");
-    String expect = String.format("Found 3 paths:\n%s\n%s\n%s", path1, path2, path3);
-    assertEquals(expect, h.handleRequest(rootPath));
-	}
-}
+    @Test
+    public void testIndex() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/");
+        assertEquals("There are 1 total files to search.", h.handleRequest(rootPath));
+    }
 
+    @Test
+    public void testSearch() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/search?q=taxation");
+        Path path1 = Paths.get("./technical/biomed/1471-2296-3-3.txt");
+        Path path2 = Paths.get("./technical/government/Gen_Account_Office/d01591sp.txt");
+        Path path3 = Paths.get("./technical/plos/journal.pbio.0020052.txt");
+        String expect = String.format("Found 3 paths:\n%s\n%s\n%s", path1, path2, path3);
+        assertEquals(expect, h.handleRequest(rootPath));
+    }
+}
